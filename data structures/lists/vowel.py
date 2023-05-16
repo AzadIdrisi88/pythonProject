7c89adcd7308208c2e4f73713ef96eb2566f5830
AzadIdrisi88 <127969229+AzadIdrisi88@users.noreply.github.com>
2023-05-16 13:31:58 +0530

    arrange by vowels

---
+++ data structures/lists/vowel.py
@@ -0,0 +1,16 @@
+a = ["apple", "mango", "orange", "cat", "deer"]
+
+
+def countVowels(x):
+    vowels = "aeiou"
+    count = 0
+    x = x.lower()
+    for ch in x:
+        if ch in vowels:
+            count += 1
+    return -count
+
+
+print(a)
+a.sort(key=countVowels)
+print(a)
